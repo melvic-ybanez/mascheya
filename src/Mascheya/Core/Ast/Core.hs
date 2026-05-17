73939fcc14d26cd34918325a2323aa92da0cbe78
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2026-05-17 19:57:47 +0800

    Lambda calculus syntax tree

--- src/Mascheya/Core/Ast/Core.hs
+++ src/Mascheya/Core/Ast/Core.hs
@@ -1,5 +1,18 @@
 module Mascheya.Core.Ast.Core where
 
-data Expr = Constant Constant
+data Expr = Var Var | Abstraction { param :: Var, body :: Expr } 
+    | Constant Constant | Application { functionName :: String, arg :: Expr } 
+    deriving Show
 
-data Constant = Int Int
+data Var = VarData { name :: String, rhs :: Expr } deriving Show
+
+data Constant = Int Int | Char Char deriving Show
+
+data BuiltinFunction = Arithmetic | Logical Logical 
+    | Boolean Boolean | If Bool Expr Expr | ListFunction ListFunction
+    deriving Show
+
+data Arithmetic = Plus | Minus | Times | Divide deriving Show
+data Logical = And | Or | Not deriving Show
+data Boolean = True | False deriving Show
+data ListFunction = Cons Expr Expr | Head Expr | Tail Expr | Nil deriving Show
