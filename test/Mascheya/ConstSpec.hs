edf69320ac6f2f28102e01efe34d28cb0064f6c3
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2026-09-08 01:46:50 +0800

    Reorganize test cases for constants

--- test/Mascheya/ConstSpec.hs
+++ test/Mascheya/ConstSpec.hs
@@ -8,6 +8,7 @@ import Test.Hspec (Spec, describe, it, shouldBe)
 spec :: Spec
 spec = do
   let runCapture = hCapture [stdout, stderr] . Runner.runDefault putStr
+
   describe "numeric value" $ do
     it "evaluates to itself" $ do
       (output, _) <- runCapture "100"
@@ -16,8 +17,11 @@ spec = do
       (output, _) <- runCapture "200.56"
       output `shouldBe` "200.56"
 
+  describe "character" $ do
+    it "evaluates to itself if simple" $ do
       (output, _) <- runCapture "'c'"
       output `shouldBe` "'c'"
 
+    it "supports control codes" $ do
       (output, _) <- runCapture "'\^A'"
       output `shouldBe` "'\\SOH'"
